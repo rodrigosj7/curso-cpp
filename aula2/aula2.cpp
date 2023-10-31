e364b4681d3d53c42025067163f03744897e00a1
rsx <contato.rodrigosilv@gmail.com>
2023-10-31 17:01:23 -0300

    first commit

---
+++ aula2/aula2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // Função padrão de saida do iostream
+    // << simbolo de saída
+  cout << "Hello World!";
+
+  system("pause");
+
+  return 0;
+}
