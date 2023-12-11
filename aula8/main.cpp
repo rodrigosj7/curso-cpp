37ff54dffce120b0f6e4df193018a853aecac56a
rsx <contato.rodrigosilv@gmail.com>
2023-12-10 23:45:43 -0300

    fix: finished classroom 8 main file

--- aula8/main.cpp
+++ aula8/main.cpp
@@ -3,7 +3,28 @@
 using namespace std;
 
 int main() {
-  cout << "Hello world";
+  int n;
+  cout << "Diga um numero inteiro nao negativo: ";
+  cin >> n;
+
+  int soma = 0;
+  int cont = 1;
+
+  // while (cont <= n) {
+  //   soma += cont;
+  //   cont ++;
+  // }
+
+  // do {
+  //   soma += cont;
+  //   cont ++;
+  // } while (cont <= n);
+
+  for (int c = 1; c <= n; c++) {
+    soma += c;
+  }
+
+  cout << "!" << "n = " << soma << endl;
 
   return 0;
 }
